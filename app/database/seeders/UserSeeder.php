4b0b92622041004ea0bc5f886e58b9246a39aae1
ito-lvgs <yuichi.ito@leverages.jp>
2020-09-18 14:53:27 +0900

    add: tenant migration and seeder

---
+++ app/database/seeders/UserSeeder.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('users')->insert([
+            'name' => Str::random(10),
+            'email' => 'test1@gmail.com',
+            'password' => Hash::make('password'),
+            'tenant_id' => 1,
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
+        DB::table('users')->insert([
+            'name' => Str::random(10),
+            'email' => 'test2@gmail.com',
+            'password' => Hash::make('password'),
+            'tenant_id' => 2,
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
+    }
+}
